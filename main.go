adf9ab6386351811bda798c2b8fa2f6e93e71271
biplavxyz <biplavgautam1@gmail.com>
2023-05-07 19:16:08 -0400

    Add Keybinds

--- main.go
+++ main.go
@@ -39,12 +39,32 @@ func main() {
 	netproc := monitor.DisplaySocks(app)
 
 	// Attach The Tabs Above To The Panels
-	panels.AddTab("ssh", "SSH", ssh)
-	panels.AddTab("NetAndProc", "Network and Processes", netproc)
-	panels.AddTab("filesystem", "Filesystem", file)
-	panels.AddTab("firewall", "Firewall", cview.NewTextView())
-	panels.AddTab("webserver", "Webserver", cview.NewTextView())
-	panels.AddTab("services", "Services", cview.NewTextView())
+	panels.AddTab("ssh", "[1] SSH", ssh)
+	panels.AddTab("NetAndProc", "[2] Network and Processes", netproc)
+	panels.AddTab("filesystem", "[3] Filesystem", file)
+	panels.AddTab("firewall", "[4] Firewall", cview.NewTextView())
+	panels.AddTab("webserver", "[5] Webserver", cview.NewTextView())
+	panels.AddTab("services", "[6] Services", cview.NewTextView())
+
+    app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+        if event.Rune() == 113 {  // 113 means q
+            app.Stop()
+        } else if event.Rune() == 49 { // 49 means 1
+            panels.SetCurrentTab("ssh")
+        } else if event.Rune() == 50 { // 50 means 2
+            panels.SetCurrentTab("NetAndProc")
+        } else if event.Rune() == 51 {
+            panels.SetCurrentTab("filesystem")
+        } else if event.Rune() == 52 {
+            panels.SetCurrentTab("firewall")
+        } else if event.Rune() == 53 {
+            panels.SetCurrentTab("webserver")
+        } else if event.Rune() == 54 {
+            panels.SetCurrentTab("services")
+        }
+
+        return event
+    })
 
 	// Set Panels as Root
 	app.SetRoot(panels, true)
